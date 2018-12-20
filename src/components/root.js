df69f17593cc07972fcc240cd63f4e29f63a0856
Kendrick Cline <kdecline@gmail.com>
2018-12-19 19:49:48 -0600

    simple todo model

--- src/components/root.js
+++ src/components/root.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react'
-import {init as firebaseInit} from '../firebase'
+import {init as firebaseInit} from './firebase'
 import {browserHistory} from 'react-router'
 import Routes from './routes'
+import {Provider} from 'react-redux'
+import configureStore from './configureStore'
+
 export default class Root extends Component {
   constructor(props) {
     super(props)
     firebaseInit()
+    this.store = configureStore()
   }
-render() {
+  render() {
     return (
-      <Routes history={browserHistory}/>
+      <Provider store={this.store}>
+        <Routes history={browserHistory}/>
+      </Provider>
     )
   }
 }
---
+++ src/firebase.js
@@ -0,0 +1,51 @@
+import * as firebase from 'firebase'
+import sectionModel from './models/section'
+import todoModel from './models/todo'
+
+let database
+export const init = () => {
+  let config = {
+    apiKey: "AIzaSyBLRZ3NCAmpwto3v18wqhr-vGliLxfYwGE",
+    authDomain: "ckeg-display.firebaseapp.com",
+    databaseURL: "https://ckeg-display.firebaseio.com",
+    projectId: "ckeg-display",
+    storageBucket: "ckeg-display.appspot.com",
+    messagingSenderId: "404124002483"
+  }
+  firebase.initializeApp(config)
+  database = firebase.database()
+}
+
+export const getSectionsDB = () => {
+  return database.ref('/').once('value')
+}
+
+// get specified section
+export const getTodoDB = (sectionId) => {
+  return database.ref(`/${sectionId}`).once('value')
+}
+
+// add new section
+export const addSection = (name) => {
+  let key = database.ref('/').push().key
+  let model = sectionModel(key, name, 
+    firebase.database.ServerValue.TIMESTAMP)
+  return database.ref('/' + key).set(model)
+}
+
+// add new todo item into specified section
+export const addTodoItem = (id, name) => {
+  return new Promise((resolve, reject) => {
+    database.ref(`/${id}`).once('value').then((todo) => {
+      let todos = todo.val().todos || []
+      let key = database.ref(`/${id}`).push().key
+
+      todos.push(todoModel(key, name,
+        firebase.database.ServerValue.TOMESTAMP))
+
+      database.ref(`/${id}/todos`).set(todos)
+        .then( res => {resolve(res)})
+        .catch( error => {reject(error)})
+    })
+  })
+}
